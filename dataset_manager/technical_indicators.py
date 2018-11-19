42812c28d6582475fd1555a9b24fe516d0161a70
Vítězslav Slavík <vitezslavik@gmail.com>
2018-11-19 21:00:58 +0100

    implement Relative Strength Index

--- dataset_manager/technical_indicators.py
+++ dataset_manager/technical_indicators.py
@@ -58,3 +58,29 @@ class TechnicalIndicators:
     def stod(dataset, d_period):
         dataset['%d'] = dataset['%k'].rolling(window=d_period).mean()
         return dataset
+
+    # RSI - Relative Strength Index
+    def rsi_indicator(self, period):
+        rsi = 'rsi{}'.format(period)
+        # Calculate differences between prices
+        deltas = np.diff(self.df['close'])
+        # For every row calculate rsi
+        for i, row in self.df.iterrows():
+            if i < period:
+                self.df.loc[i, rsi] = 0
+            else:
+                self.df.loc[i, rsi] = self.calc_rsi(i, period, deltas)
+        return self
+
+    @staticmethod
+    def calc_rsi(index, period, deltas):
+        seed = deltas[index - period:index]
+        average_gain = seed[seed >= 0].sum() / period
+        average_loss = seed[seed < 0].sum() / period
+
+        if abs(average_loss) == 0:
+            rs = 0
+        else:
+            rs = average_gain / abs(average_loss)
+        rsi = 100. - (100. / (1 + rs))
+        return rsi
