fe3b05ea5cb5b748b2c368a34d39791727d018de
Vítězslav Slavík <vitezslavik@gmail.com>
2018-11-20 20:22:20 +0100

    add ModelBuilder attributes

--- neural_lab/model_preset.py
+++ neural_lab/model_preset.py
@@ -1,98 +1,65 @@
+# Imports
+import numpy as np
 # Building Neural Network
-# Import
+from keras import metrics
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import BatchNormalization, Flatten
 from keras.layers.advanced_activations import LeakyReLU, ELU
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 
+
 class ModelBuilder:
 
     def __init__(self):
-        self.model_name: str = None
-        self.compiled_model = None
-        self.trained_model = None
-        self.history = None
+        # self.model_name: str = None
+        # self.compiled_model = None
+        # self.trained_model = None
+        # self.history = None
+        #
+        # # VECTOR SEQUENCES
+        # self.n_past: int = None
+        # self.n_future: int = None
+        #
+        # self.do_batch_norm = None
+        #
+        # # Scoring
+        # self.monitor_metric: str = None
+        # self.val_monitor_metric = 'val_' + self.monitor_metric
+        # self.train_score: float = None
+        # self.val_score: float = None
+        # self.val_score_in_epoch_n: int = None
+        # self.test_score: float = None
 
-        # VECTOR SEQUENCES
-        self.n_past: int = None
-        self.n_future: int = None
+        # GENERAL
+        self.starting_learn_rate = None
+        self.optimizer: str = 'Adam'
+        self.model_name: str = 'model_ann'
+        self.val_split: float = 0.5
 
-        self.do_batch_norm = None
+        # PROPERTIES
+        self.epochs: int = 100
+        self.loss_func: str = 'binary_crossentropy'
+        self.batch_size: int = 32
+        self.starting_learn_rate: float = 0.01
 
-        # Scoring
-        self.monitor_metric: str = None
-        self.val_monitor_metric = 'val_' + self.monitor_metric
-        self.train_score: float = None
-        self.val_score: float = None
-        self.val_score_in_epoch_n: int = None
-        self.test_score: float = None
+        # BEHAVIORS
+        self.shuffle_inputs: bool = False
+        self.do_batch_norm: bool = False
+        # Dropout
+        self.dropout_rate: float = 0.5
+        self.do_dropout: bool = False
+        # Dynamic Learning Rate
+        self.learning_rate_patience: int = 0
+        self.learning_rate_reduction: float = 0.90
+        # Dynamic Training Stop
+        self.stop_training_patience = 50
 
-    # MODEL WORKFLOW
-    # ------------------------------------------------------------------------------
-    # Compile model + load saved weights
-    def load_network(self):
-        self.trained_model = self.build_network()
-        self.trained_model.load_weights(
-            filepath='trained_models/{}/{}.hdf5'.format(self.model_name, self.model_name),
-            by_name=False)
-        return self.trained_model
+        # SCORING
+        self.monitor_metric: str = 'acc'
+        self.val_monitor_metric: str = 'val_' + self.monitor_metric
+        self.train_score: float = 0.
+        self.val_score: float = 0.
+        self.val_score_max_in_epoch: int = 0
 
-    # Compile model + retrain model
-    def train_network(self, X_train, y_train):
-        self.trained_model = self.build_network()
-        # Callbacks
-        stop_training = EarlyStopping(monitor=self.val_monitor_metric, min_delta=1e-10,
-                                      patience=self.stop_patience,
-                                      verbose=1)
-        reduce_learning_rate = ReduceLROnPlateau(monitor=self.val_monitor_metric,
-                                                 factor=self.learn_rate_reduce,
-                                                 patience=self.learn_rate_patience,
-                                                 min_lr=0.000001, verbose=1)
-        self_checkpoint = ModelCheckpoint(
-            filepath='trained_models/{}/{}.hdf5'.format(self.model_name, self.model_name),
-            monitor=self.val_monitor_metric,
-            verbose=1, save_best_only=True)
-        # tensor_board = TensorBoard(log_dir='{}'.format(self), write_graph=True, write_images=True)
-        self.history = self.trained_model.fit(X_train, y_train, shuffle=self.shuffle,
-                                              epochs=self.epochs,
-                                              callbacks=[stop_training, reduce_learning_rate,
-                                                         self_checkpoint],
-                                              validation_split=self.val_split, verbose=1,
-                                              batch_size=self.batch_size)
-
-        self.val_metric = np.max(self.history.history[self.val_monitor_metric], axis=0)
-        self.val_metric_in_epoch_n = np.argmax(self.history.history[self.val_monitor_metric],
-                                               axis=0)
-        self.train_metric = self.history.history[self.monitor_metric][
-            self.val_metric_in_epoch_n]
-
-        # Rounding Score
-        if self.monitor_metric is 'acc':
-            self.train_metric = (self.train_metric * 100).round(2)
-            self.val_metric = (self.val_metric * 100).round(2)
-        else:
-            self.train_metric = self.train_metric.round(6)
-            self.val_metric = self.val_metric.round(6)
-
-        return self.trained_model, self.history
-
-    # MODEL BUILDING TASKS
-    # ------------------------------------------------------------------------------
-    # Add Dropout layer inside model
-    def add_dropout(self, model):
-        if self.do_dropout:
-            model.add(Dropout(self.dropout_num))
-            return model
-
-    # Batch Normalization - Apply Z-score on inputs inside NN
-    def add_batch_norm(self, model):
-        if self.do_batch_norm:
-            model.add(BatchNormalization())
-            return model
-
-    # Add layer for flattening the dimension of input
-    @staticmethod
-    def add_flat(model):
-        model.add(Flatten())
-        return model
+    
