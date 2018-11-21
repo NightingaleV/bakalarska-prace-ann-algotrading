8fd864c6f0f8c6902e20dce0243ce15617d3a040
Vítězslav Slavík <vitezslavik@gmail.com>
2018-11-22 00:12:35 +0100

    add indicators to iterator

--- run_files/training_iterator.py
+++ run_files/training_iterator.py
@@ -8,6 +8,7 @@ import gc
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+
 # Custom Packages
 nb_dir = os.path.split(os.getcwd())[0]
 if nb_dir not in sys.path:
@@ -40,20 +41,40 @@ predictions_periods = [
     [50, 10],
 ]
 # Postfix for multiple iterations of one model setup
-multiple_iteration = ['iter-1', 'iter-2']
+multiple_iterations = ['iter-1', 'iter-2']
 
-# ITERATOR PARAMETERS
+# ITERATOR
 # ------------------------------------------------------------------------------
-for moving_average, periods, iteration in itertools.product(moving_average_periods,
-                                                            predictions_periods,
-                                                            multiple_iteration):
-
+# For exporting Results
+models_evaluations = []
+for moving_average, periods, iteration_postfix in itertools.product(moving_average_periods,
+                                                                    predictions_periods,
+                                                                    multiple_iterations):
     # IMPORT NEURAL NETWORK
     # --------------------------------------------------------------------------
     model = ModelNeuralNetwork(data_manager=dm)
-    model.predict_ma: int = 30
-    model.n_past: int = 25
-    model.n_future: int = 5
+    model.predict_ma: int = moving_average
+    model.n_past: int = periods[0]
+    model.n_future: int = periods[1]
     model.model_task: str = 'classification'
-    model.model_postfix: str = ''
-    models_evaluations = []
+    model.model_postfix: str = iteration_postfix
+
+    # INDICATORS
+    # --------------------------------------------------------------------------
+    dm.restore_df()
+    dm.ewma(model.predict_ma)
+    if moving_average != 15:
+        dm.ewma(15)
+    if moving_average != 20:
+        dm.ewma(20)
+    if moving_average != 30:
+        dm.ewma(30)
+    if moving_average != 40:
+        dm.ewma(40)
+    if moving_average != 60:
+        dm.ewma(60)
+    dm.rsi_indicator(25)
+    dm.stochastic_oscilator(25, 3, 3)
+    dm.get_indicators(target=model.model_task)
+
+    
