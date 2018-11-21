5741d4f76ac27178a222f3425a28b842cc0e3198
Vítězslav Slavík <vitezslavik@gmail.com>
2018-11-21 19:16:45 +0100

    create sets for evaluation

--- neural_lab/model_evaluation.py
+++ neural_lab/model_evaluation.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 class ModelEvaluation:
 
-    def __init__(self):
+    def __init__(self, data_manager):
+        # TODO Inherit Attributes For reference
+        # Calculated by DataManager in test_train_split
+        self.data_manager = data_manager
+
         # Accuracy
         self.test_score = 0
 
@@ -12,7 +16,42 @@ class ModelEvaluation:
         self.rmse_train = 0
         self.rmse_test = 0
 
+    # CREATE SETs FOR EVALUATION
+    # ------------------------------------------------------------------------------
+    @staticmethod
+    def create_test_eval_set(np_close, np_actual_test, np_prediction_test):
+        df_test_eval = pd.DataFrame(data=np.hstack((np_close, np_actual_test, np_prediction_test)),
+                                    columns=['close', 'actual', 'prediction'])
+        df_test_eval.reset_index(drop=True, inplace=True)
+        return df_test_eval
+
+    def create_train_eval_set(self, np_actual_train, np_predictions_train):
+        # TRAIN Dataframe for Evaluation - Close price, Actual / Prediction
+        df_train_eval = pd.DataFrame(data=np.hstack((np_actual_train, np_predictions_train)),
+                                     columns=['actual', 'prediction'])
+        # Add close prices
+        df_train_eval['close'] = self.data_manager.df['close'][self.n_past:].reset_index(drop=True)
+        # Slice DataFrame - Only Train set without validation part
+        index_start = int(self.data_manager.train_rows * 0)
+        index_end = int(self.data_manager.train_rows * (1 - self.val_size))
+        df_train_eval = df_train_eval[index_start:index_end]
+        df_train_eval.reset_index(drop=True, inplace=True)
+        return df_train_eval
+
+    def create_val_eval_set(self, np_actual_train, np_predictions_train):
+        # VALIDATION Dataframe for Evaluation - Close price, Actual / Prediction
+        df_val_eval = pd.DataFrame(data=np.hstack((np_actual_train, np_predictions_train)),
+                                   columns=['actual', 'prediction'])
+        df_val_eval['close'] = self.data_manager.df['close'][self.n_past:].reset_index(drop=True)
+        # Slice DataFrame - Only Validation set without train part
+        index_start = int(self.data_manager.train_rows * (1 - self.val_size))
+        index_end = int(self.data_manager.train_rows * 1)
+        df_val_eval = df_val_eval[index_start:index_end]
+        df_val_eval.reset_index(drop=True, inplace=True)
+
     # PREDICTION ACCURACY
+    # OTHER
+    # ------------------------------------------------------------------------------
     @staticmethod
     def calc_acc(dataset, origin=0, actual_slope='actual', predicted_slope='prediction'):
         # True if actual are in same direction as predictions
